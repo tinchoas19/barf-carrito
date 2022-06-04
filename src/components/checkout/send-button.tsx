827c28de578ff3a849f2dc10c178c222b46fee28
Manuel <manuelrmauro@gmail.com>
2022-06-03 23:48:27 -0300

    feat/checkout

--- src/components/checkout/send-button.tsx
+++ src/components/checkout/send-button.tsx
@@ -1,13 +1,5 @@
-import { formatOrderedProduct } from '@/lib/format-ordered-product';
-import { useAtom } from 'jotai';
-import { billingAddressAtom, shippingAddressAtom } from '@/store/checkout';
 import Button from '@/components/ui/button';
-import { useCart } from '@/store/quick-cart/cart.context';
 import classNames from 'classnames';
-import { useVerifyOrder } from '@/framework/order';
-import omit from 'lodash/omit';
-
-
 
 export const SendButton: React.FC<{callback:Function, disabled?:boolean, label:string}> = (
   {callback, disabled, label}) => {
