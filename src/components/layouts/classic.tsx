459a4a22b89f6199acf1359910b509e56e7750ca
Martin Benzo <martinbenzo@gmail.com>
2023-07-08 20:03:05 -0300

    added alert into home screen

--- src/components/layouts/classic.tsx
+++ src/components/layouts/classic.tsx
@@ -5,20 +5,38 @@ import { Element } from 'react-scroll';
 import FilterBar from './filter-bar';
 import ProductGridHome from '@/components/products/grids/home';
 import type { HomePageProps } from '@/types';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useSettings } from '@/framework/settings';
+import Cookies from 'js-cookie';
 
 export default function ClassicLayout({ variables }: HomePageProps) {
  
   const [showSlider, setShowSlider] = useState(false)
+  const [showAlert, setShowAlert] = useState(true)
+
+  const {
+    settings: { mensajehome },
+  } = useSettings();
+
 
   useEffect(() => {
+    console.log(mensajehome)
+    if(mensajehome.activo == "1" && Cookies.get("alert") != "ok") {
+      var now = new Date();
+      now.setTime(now.getTime() + 1 * 3600 * 1000);
+      let expireTimeStamp = parseInt((now.getTime() / 1000).toString());
+      Cookies.set("alert", "ok", { expires: expireTimeStamp });
+     
+      window.alert(mensajehome.mensaje);
+  }
     setTimeout(() => {
       setShowSlider(true)
     },500)
-  }) 
+  },[]) 
 
   return (
     <>
+    
       <Banner layout="classic" variables={variables.types} />
       {showSlider && <PromotionSliders variables={variables.types} />}
       
